b990ab88bbf35d3587560fcaa5f06590d184a09f
blacklight <d3bugalg@gmail.com>
2025-08-04 15:31:23 +0100

    point de sauvegarde

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -21,11 +21,86 @@ return {
   -- Noice.nvim : UI moderne pour messages, notifications, LSP, historique, etc.
   {
     "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim", -- Dépendance obligatoire pour les fenêtres flottantes
+    opts = function(_, opts)
+      opts.routes = opts.routes or {}
+      opts.presets = opts.presets or {}
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with `:Noice`
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 1000,
+      render = "wrapped-compact",
+      max_width = 20
     },
-    opts = {}, -- Config de base, personnalisable ensuite
-    
+    config = function(_, opts)
+      local notify = require("notify")
+      -- Patch global : toutes les notifications restent si la souris est dessus et max_width=60 par défaut
+      vim.notify = function(msg, level, user_opts)
+        user_opts = user_opts or {}
+        user_opts.keep = user_opts.keep or function()
+          local mouse = vim.fn.getmousepos()
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local config = vim.api.nvim_win_get_config(win)
+            if config.relative == "editor" and config.zindex and config.zindex > 150 then
+              if mouse.winid == win then
+                return true
+              end
+            end
+          end
+          return false
+        end
+        user_opts.max_width = user_opts.max_width or 60
+        return notify(msg, level, user_opts)
+      end
+    end,
   },
 }
